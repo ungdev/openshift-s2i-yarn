4a24f6fd7f852369a6b52d029b03e145eadedd42
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-02-23 18:42:13 +0100

    Travis added

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+name='ungdev/openshift-s2i-yarn'
+tags=(lts-alpine alpine 13-alpine 12-alpine 10-alpine)
+
+for tag in ${tags[*]}; do
+  echo "Create image node:$tag"
+  echo "FROM node:$tag" > Dockerfile
+  cat Dockerfile.template >> Dockerfile
+
+  if [[ $tag == 'alpine' ]]; then
+    tag='latest'
+  fi
+
+  # Remove tag name
+  tag=${tag%-alpine}
+
+  docker build -t $name:$tag .
+  docker push $name:$tag
+done
